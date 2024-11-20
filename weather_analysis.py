13a01782f3bc7e83f67c45953fa7fa9e8b427484
Deirdre Cameron-McKenna <acameronmcke@mun.ca>
2024-11-19 21:17:44 -0330

    added functions for rain and snow

--- weather_analysis.py
+++ weather_analysis.py
@@ -1,5 +1,6 @@
 from datetime import date
 import pandas as pd
+import numpy as np
 weather_data = pd.read_csv('en_climate_daily_NL_8403505_2020_P1D.csv', usecols=range(4,31), index_col='Date/Time')
 
 
@@ -17,5 +18,42 @@ def monthly_mean_temp(dataframe, month):
     print(actualmean)
     return actualmean
 
-print(monthly_mean_temp(weather_data, 1))
+def yearly_mean_temp(dataframe):
+    monthly_mean = []
+    for i in range(11):
+        print(i)
+        monthly_mean.append(monthly_mean_temp(dataframe, i+1))
+    
+    return monthly_mean
 
+def monthly_rainy_days(dataframe, month):
+    weather_data = dataframe
+    num_cols_rain = ['Total Rain (mm)']
+    weather_data[num_cols_rain] = weather_data[num_cols_rain].astype(float)
+    total_rain = weather_data[num_cols_rain]
+
+
+    first_day = (date(2020, month, 1) - date(2020, 1, 1)).days
+    last_day = (date(2020, month+1, 1) - date(2020, 1, 1)).days - 1
+
+    rainy_days = np.count_nonzero(total_rain[first_day:last_day])
+    rain_amount = total_rain[first_day:last_day].sum()
+    return rainy_days, rain_amount
+
+def monthly_snowy_days(dataframe, month):
+    weather_data = dataframe
+    num_cols_snow = ['Total Snow (cm)']
+    weather_data[num_cols_snow] = weather_data[num_cols_snow].astype(float)
+    total_snow = weather_data[num_cols_snow]
+
+    first_day = (date(2020, month, 1) - date(2020, 1, 1)).days
+    last_day = (date(2020, month+1, 1) - date(2020, 1, 1)).days - 1
+
+    snowy_days = np.count_nonzero(total_snow[first_day:last_day])
+    snow_amount = total_snow[first_day:last_day].sum()
+    return snowy_days, snow_amount
+
+
+#print(monthly_mean_temp(weather_data, 1))
+#print(yearly_mean_temp(weather_data))
+print(monthly_snowy_days(weather_data, 1))
